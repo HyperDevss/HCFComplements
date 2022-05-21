8d6f9ff1bd24f474aac6642a6d82074a62a16e31
zD3motead0 <100808972+zD3motead0@users.noreply.github.com>
2022-05-21 17:41:42 -0500

    Upload Main.php

---
+++ src/Prxzy/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Prxzy;
+
+use Prxzy\command\RenameCommand;
+use Prxzy\command\FeedCommand;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase {
+  
+  public function onEnable() : void {
+    # registro de comandos
+    $this->getServer()->getCommandMap()->register("rename", new RenameCommand());
+    $this->getServer()->getCommandMap()->register("feed", new FeedCommand());
+    
+    $this->getLogger()->info(TextFormat::colorize("§aPlugin Activado"));
+  }
+  
+  public function onDisable() : void {
+    $this->getLogger()->info(TextFormat::colorize("§cPlugin Desactivado"));
+  }
+    
+}
