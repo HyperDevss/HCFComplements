145690688428fc6a32f48c68dec0ed3c5e68fcc2
tbralex7@gmail.com <tbralex7@gmail.com>
2022-05-30 15:56:12 -0500

    Update Ender Command

--- src/HCFCom/command/EnderChestCommand.php
+++ src/HCFCom/command/EnderChestCommand.php
@@ -18,4 +18,18 @@ class EnderChestCommand extends Command {
   public function __construct(){
     parent::__construct("ender", "Open EnderChest", null, ["enderchest", "chest", "ender"]);
   }
+  
+  public function execute(CommandSender $pl, string $label, array $args){
+    if($pl instanceof Player){
+       $menu = InvMenu::create(InvMenuTypeIds::TYPE_CHEST);
+       $menu->setName("§5EnderChest");
+       $inv = $menu->getInventory();
+       $inv->setContents($sender->getEnderInventory()->getContents());
+       $menu->setListener(function (InvMenuTransaction $transaction) use ($sender): InvMenuTransactionResult{
+       $sender->getEnderInventory()->setItem($transaction->getAction()->getSlot(), $transaction->getIn());
+       return $transaction->continue();
+       });
+       $menu->send($sender);
+    }
+  }
 }
